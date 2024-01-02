231b169ff5bacbb458dfbc678761d83503ffba8b
Koroem <conduratvictor@yahoo.com>
2024-01-02 13:13:02 +0200

    obtine continut pagina

---
+++ main.py
@@ -0,0 +1,13 @@
+import requests
+from bs4 import BeautifulSoup
+from urllib.parse import urljoin
+
+def obtine_continut_pagina(url):
+    """
+    Face o cerere HTTP și returnează conținutul paginii.
+    """
+    try:
+        response = requests.get(url)
+        return response.content
+    except requests.RequestException as e:
+        raise Exception(f"Eroare la accesarea URL-ului {url}: {e}")
